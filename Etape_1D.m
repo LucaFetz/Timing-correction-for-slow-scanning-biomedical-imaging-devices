0ee1c265d56f4bf3c6529f951970e8b783dce3c7
LucaFetz <luca.fetz@epfl.ch>
2019-03-18 21:07:23 +0100

    Révision de la cohérence des matrices/vecteurs
    Révision des indices, maintenant C ressemble presque à une double gaussienne.

--- Etape_1D.m
+++ Etape_1D.m
@@ -22,80 +22,97 @@ dx = (0:1:Nx-1)';
 %xImageStacked = repmat(xImage,1,Frames); %stack of Frames xImages
 dt = (0:sampling_time:(Nx*Frames-1)*sampling_time)';
 %t_fluctuation = gaussmf(dt, [2 5]); % fluctuation of xImage over time
-df0 = f0(repmat(dx',1,1),repmat(dt,1,Nx)); %sampled f0. each row is the image in a certain timeframe
-df1 = f0(repmat(dx',Nx*Frames,Frames),repmat(dt,1,Nx*Frames)); %sampled f0. In reality only the first Nx columns are needed. This is used to simplify measurement with diag(df1)
-
+df0 = f0(dx,repmat(dt',Nx,1)); %sampled f0. each column is the image in a certain timeframe
+df1 = repmat(df0,Nt,1); %sampled f0. In reality only the first Nx columns are needed. This is used to simplify measurement with diag(df1)
+df2 = f0(dx,1:Nt);
 %% measurement and usual approximation
 measurement = diag(df1); %each sample is taken from its timeframe. In usual approximation it is considered as image at time t=0
 figure
-plot(repmat(dx,Frames,1), [measurement df1(1,:)' df1(floor(end/2),:)']);
+plot(repmat(dx,Frames,1), [measurement reshape(df0(:,1:Nt:Nx*(Nt-1)),Nx*Nt,1) reshape(df0(:,(1:Nt:Nx*(Nt-1))+floor(Nt/2)),Nx*Nt,1)]);
 legend(' superposition of Measurements / usual approximation','Ground truth at t=0','Ground truth at time t=1/2')
 mean_measurement = mean(reshape(measurement,Nx,Frames),2);
 figure
-plot(dx, [mean_measurement df1(1,1:Nx)' df1(floor(end/2),1:Nx)']);
-legend(' Mean of Measurements / usual approximation','Ground truth at t=0','Ground truth at time t=1/2')
+plot(dx, [mean_measurement mean(df0(:,1:Nt:Nx*(Nt-1)),2) mean(df0(:,(1:Nt:Nx*(Nt-1))+floor(Nt/2)),2)]);
+legend(' Mean of Measurements / usual approximation','Mean of Ground truth at t=integer','Mean of Ground truth at time t=integer+1/2')
 
 figure 
 for i=1:Frames
-    plot(dx, [measurement((i-1)*Nx+1:i*Nx) df1(i,(i-1)*Nx+1:i*Nx)']);
+    plot(dx, [measurement((i-1)*Nx+1:i*Nx) df0(:,(i-1)*Nt+1)]); %measurement of image i, ground truth at time i
     legend(' Measurement / usual approximation','Ground truth')
     title(i)
-    pause(0.001)
+    pause(0.01)
 end
 
 %% forward model
 %continuous forward model
 k = repmat(0:1:Nx-1,Nt,1);
 l = repmat((0:1:Nt-1)',1,Nx);
-h = @(x,t) (B_spline(t-l).*B_spline(x-k));
-H = zeros(Nx*Nt,Nx*Nt);
-for i = 0:1:Nx-1
-    for j = 0:1:Nt-1 
-       H(i*(Nx-1)+(j+1),:) = reshape(h(i,j+i*sampling_time),1,Nx*Nt);
+%h rows are fixed in time and columns are fixed in space. represent result
+%of B_splines for a given pixel
+h = @(x,t) (B_spline(x-k).*B_spline(t-l));%takes scalar input and gives matrix output (h(x-0,t-0), h(x-1,t-0),..h(x-(Nx-1),t-(Nt-1))
+
+H = zeros(Nx*Nt,Nx*Nt); % each row of H is h unfolded.
+index = 0;
+for j = 0:1:Nt-1 
+    for i = 0:1:Nx-1
+        %the reshape on h is made to keep the rows of h(x,t) intact and
+        %after the reshape we have h(x,t)=[h(x-0,t-0) h(x-1,t-0) h(x-2,t-0)...
+        %.. h(x-(Nx-1),t-0) h(x-0,t-1) h(x-1,t-1) ... h(x-(Nx-1),t-(Nt-1))]
+        index = index+1;
+       H(index,:) = reshape(h(i,j+i*sampling_time)',1,Nx*Nt); %is it better to have the double diag? can be done keeping the columns intact (juste remove')
+%rows of H are:
+%H=[h(0,0);h(1,sampling_time);h(2,2*sampling_time);...;h((Nx-1),(Nx-1)*sampling_times);
+%h(1,1+sampling_time);h(2,1+2*sampling_time);...;h((Nx-1),(Nx-1)*sampling_time+(Nt-1))]
     end
 end
-y = diag(measurement);
+y = diag(measurement); %
 
 %% check conditionement of H
 %visualize
+figure
 imagesc(H);
 condition = cond(H); %bad condition
 %% coefficient optimization for B spline interpolation
 %naive inverse approach
 C = H\measurement; %inv(H)*measurement;
-C = reshape(C,Nt,Nx);
+C = reshape(C,Nx,Nt)';
 imdisp(C,'C found by Naive approach',1);
 %optimization approach
 
 %Makes sense to me but commented because dimensions don't match
-% index = 1;
-% H2 = LinOpBroadcastMatrix(H,size(measurement),index);  %not really sure how to put H to the good format
-% %H2 = LinOpDiag(size(H)); %test. not a good H
-% LS=CostL2([],measurement);  % Least-Sqaures data term
-% F=LS*H2; %composition of cost and H
-% 
-% GD=OptiGradDsct(F);
-% GD.OutOp=OutputOpti(1,df0,40);
-% GD.ItUpOut=2;           % call OutputOpti update every ItUpOut iterations
-% GD.maxiter=200;         % max number of iterations
-% GD.run(measurement); % run the algorithm (Note that gam is fixed automatically to 1/F.lip here since F.lip is defined and since we do not have setted gam) 
-
-%Dimensions match but not sure it has any meaning
-index = 1;
-H2 = LinOpBroadcastMatrix(H,size(measurement),index);  %not really sure how to put H to the good format
-%H2 = LinOpDiag(size(H)); %test. not a good H
+H2 = LinOpMatrix(H);  %not really sure how to put H to the good format
 LS=CostL2([],measurement);  % Least-Sqaures data term
 F=LS*H2; %composition of cost and H
 
 GD=OptiGradDsct(F);
-GD.OutOp=OutputOpti(1,df0(:,1),40); %Why take only part of ground truth df0?? Dimension match but no meaning
+GD.OutOp=OutputOpti(1,[],40); %prendre df0 avec un pas de 1 et pas dt
 GD.ItUpOut=2;           % call OutputOpti update every ItUpOut iterations
 GD.maxiter=200;         % max number of iterations
 GD.run(measurement); % run the algorithm (Note that gam is fixed automatically to 1/F.lip here since F.lip is defined and since we do not have setted gam) 
 
-C2 = reshape(GD.xopt,Nt,Nx);
+%Dimensions match but not sure it has any meaning
+
+% H2 = LinOpBroadcastMatrix(H);  %not really sure how to put H to the good format
+% %H2 = LinOpDiag(size(H)); %test. not a good H
+% LS=CostL2([],measurement);  % Least-Sqaures data term
+% F=LS*H2; %composition of cost and H
+% 
+lambda = 3; %thuner la régul
+R = CostL2(size(measurement)); %regulation term. should it be a Cost? plutôt size(C)
+F2 = F+lambda*R; %how to integrate the regulation term?
+%regarder evolcost pour voir nb iterations
+
+% 
+% GD=OptiGradDsct(F);
+% GD.OutOp=OutputOpti(1,df0(:,1),40); %Why take only part of ground truth df0?? Dimension match but no meaning
+% GD.ItUpOut=2;           % call OutputOpti update every ItUpOut iterations
+% GD.maxiter=200;         % max number of iterations
+% GD.run(measurement); % run the algorithm (Note that gam is fixed automatically to 1/F.lip here since F.lip is defined and since we do not have setted gam) 
+
+C2 = reshape(GD.xopt,Nx,Nt)';
 %% Display
 imdisp(C2,'C found by Gradient Descent',1);
+
 
 %% reconstruction
 reconst = H*reshape(C2,Nx*Nt,1); 
